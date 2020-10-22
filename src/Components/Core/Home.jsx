9d3498fec06f1cac695c06a226dbaad6a560985c
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-23 03:56:42 +0600

    Create Route app and setup routers

---
+++ src/Components/Core/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center m-4 text-info">Hello from Home Component</h1>
+        </div>
+    )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "../src/main.scss";
-import App from './App';
 import './index.css';
+import Routes from "./Routes";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Routes/> ,document.getElementById('root'));
