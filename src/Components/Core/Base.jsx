f6fba57b99f48a3d13824deeae064db0e8df5013
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-24 03:49:26 +0600

    create reusable card component

--- src/Components/Core/Base.jsx
+++ src/Components/Core/Base.jsx
@@ -5,14 +5,14 @@ const Base = ({title="myTitle", description="my description", className="bg-dark
         <>
             <div className="container-fluid">
                 <div className="jumbotron bg-dark text-white text-center">
-                    <h2 className="display-4"> {title} </h2>
+                    <h2 className="display-4 text-center"> {title} </h2>
                     <p className="lead"> {description} </p>
                 </div>
                 <div className={className}> {children} </div>
             </div>
             <footer className="footer bg-dark mt-auto py-3">
                 <div className="container-fluid bg-success text-white text-center py-3">
-                    <h4>If you got any question reach our mail</h4>
+                    <h4>If you got any question contact our mail</h4>
                     <button className="btn-lg btn-warning btn">Contact Us</button>
                     <div className="container">
                         <span className="text-muted">
--- src/Components/Core/Card.jsx
+++ src/Components/Core/Card.jsx
@@ -1,34 +1,75 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import ImageHelper from './Helper/ImageHelper';
 
-const Card = ({product, addToCart=true, removeFromCart=false}) => {
-    return (
-      <div className="card text-white bg-dark border border-info ">
-        <div className="card-header lead">A photo from pexels</div>
-        <div className="card-body">
-          <div className="rounded border border-success p-2">
-            <ImageHelper product={product}/>
-          </div>
-          <p className="lead bg-success font-weight-normal text-wrap">
-            this photo looks great
-          </p>
-          <p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
-          <div className="row">
-            <div className="col-12">
-              <button
-                onClick={() => {}}
+const isAuthenTicated = false
+
+const Card = ({product, addTocart=true, removeFromCart=false}) => {
+    const cartTitle = product ? product.name : "Anonymous";
+    const cartDescription = product ? product.description : "defaoult description"
+    const cartPrice = product ? product.price : "0.0"
+
+    const addToCart = () => {
+        if(isAuthenTicated) {
+            console.log("Add to cart");
+        }else {
+            console.log("log in first");
+        }
+    }
+
+    const getRedirect = (redirect) => {
+        if(redirect) {
+            return <Redirect to="/cart" />
+        }
+    }
+
+    const showAddToCart = addTocart => {
+        return (
+            addTocart && (
+                <button
+                onClick={addToCart}
                 className="btn btn-block btn-outline-success mt-2 mb-2"
               >
                 Add to Cart
               </button>
-            </div>
-            <div className="col-12">
-              <button
-                onClick={() => {}}
+            )
+        )
+    }
+    const showRemoveFromCart = (removeFromCart) => {
+        return (
+            removeFromCart && (
+                <button
+                onClick={() => {
+                    console.log("product remove from cart");
+                }}
                 className="btn btn-block btn-outline-danger mt-2 mb-2"
               >
                 Remove from cart
               </button>
+            )
+        )
+
+    }
+
+
+
+    return (
+      <div className="card text-white bg-dark">
+        <div className="card-header lead text-center"> {cartTitle} </div>
+        <div className="card-body">
+          <div className="p-2">
+            <ImageHelper product={product}/>
+          </div>
+          <p className="lead bg-success font-weight-normal text-wrap text-center">
+            {cartDescription}
+          </p>
+            <p className="btn btn-success rounded  btn-sm px-4">Tk{ cartPrice }</p>
+          <div className="row">
+            <div className="col-12">
+              {showAddToCart(addToCart)}
+            </div>
+            <div className="col-12">
+              {showRemoveFromCart(removeFromCart)}
             </div>
           </div>
         </div>
