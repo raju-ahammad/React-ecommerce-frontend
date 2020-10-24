3a294067f080e0720a6e9e9abfe9281757dc8fa5
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-24 23:44:05 +0600

    create sign OUt method

--- src/Components/Auth/Helper/Helper.jsx
+++ src/Components/Auth/Helper/Helper.jsx
@@ -1,4 +1,5 @@
 import { API } from "../../../Api";
+import { cartEmpty } from "../../Core/Helper/CartHelper";
 
 
 export const signUp = (user) => {
@@ -14,4 +15,57 @@ export const signUp = (user) => {
         return response.json()
     })
     .catch((err) => console.log(err));
+}
+
+export const signIn = (user) => {
+    const formData = new FormData()
+    for (const name in user) {
+        formData.append(name, user[name]);
+    }
+    return fetch(`${API}user/login/`, {
+        method: "POST",
+        
+        body: formData,
+    })
+    .then((response)=> {
+        return response.json()
+    })
+    .catch((err) => console.log(err));
+}
+
+export const authenticate = (data, next) => {
+    if( typeof window !== undefined ) {
+        localStorage.setItem("jwt",JSON.stringify(data));
+        next();
+    }
+}
+
+export const isAuthenticated = () => {
+    if (typeof window == undefined) {
+        return false;
+
+    }
+    if (localStorage.getItem("jwt")) {
+        return JSON.parse(localStorage.getItem("jwt"));
+    }else {
+        return false
+    }
+}
+
+export const signOut = (next) => {
+    const userId = isAuthenticated() && isAuthenticated().user.id
+
+    if(typeof window !== undefined) {
+        localStorage.removeItem("jwt")
+        cartEmpty(()=>{})
+
+        return fetch(`${API}user/logout/${userId}`, {
+            method: "GET",
+        })
+        .then((response)=> {
+            console.log("Sign Out Success");
+            next()
+        })
+        .catch((err) => console.log(err));
+    }
 }
