9cc801fee13a13198be30413f67a55ebe530e9cd
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-25 19:55:37 +0600

    create  signin page

---
+++ src/Components/User/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from '../Core/Base'
+
+const SignIn = () => {
+    return (
+        <Base title="this Sign in Page" description="T shirt Store">
+            <h1>Welcome to sign in page</h1>
+        </Base>
+    )
+}
+
+export default SignIn
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import PrivateRouter from './Components/Auth/Helper/PrivateRouter'
 import Home from './Components/Core/Home'
 import SinglePage from './Components/Core/SinglePage'
+import SignIn from './Components/User/SignIn'
 import SignUp from './Components/User/SignUp'
 import UserDashboard from './Components/User/UserDashboard'
 
@@ -13,6 +14,7 @@ const Routes = () => {
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/signup" exact component={SignUp} />
+      <Route path="/signin" exact component={SignIn} />
       <Route path="/detail/:slug" exact component={SinglePage} />
       <PrivateRouter path="/user/dashboard/" exact component={UserDashboard} />
     </Switch>
