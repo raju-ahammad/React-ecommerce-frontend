927b25f73ac2287fec03d945e779df2e8d752a06
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-26 22:00:30 +0600

    create cart and check out

--- src/Components/Core/Cart.jsx
+++ src/Components/Core/Cart.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Base from './Base'
+import Card from './Card'
+import { loadCart } from './Helper/CartHelper'
+
 
 const Cart = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(()=> {
+        setProducts(loadCart());
+    }, [])
+
+    const loadProduct = (products) => {
+        return (
+            <div>
+                {products.map((product, index) => {
+                    return <Card keu={index} product={product} removeFromCart={true} addTocart={true} />
+                })}
+            </div>
+        )
+    }
+
+    const checkoutProduct = () => {
+        return (
+            <div>
+                Check out
+            </div>
+        )
+    }
+
     return (
        <Base title="cart page"  description="cart page">
-       <h1>This is Cart page</h1>
+       <div className="row">
+           <div className="col-6">
+               {loadProduct(products)}
+           </div>
+           <div className="col-6">
+               {checkoutProduct()}
+           </div>
+       </div>
        </Base>
     )
 }
