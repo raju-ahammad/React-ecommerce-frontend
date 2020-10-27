e7a9d17ff46cbbd4637b3b01f61761b9a8c18081
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-27 18:03:20 +0600

    Updates

---
+++ precache-manifest.e7b7774cbe1bd3a2b2e11f54f3ff6475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f194d116426e99783d1c6f242e9bd48",
+    "url": "/raju-ahammad/React-ecommerce-frontend/index.html"
+  },
+  {
+    "revision": "21694be47c8d4f5275c0",
+    "url": "/raju-ahammad/React-ecommerce-frontend/static/css/main.4c156203.chunk.css"
+  },
+  {
+    "revision": "a095f4a258e08088f7fc",
+    "url": "/raju-ahammad/React-ecommerce-frontend/static/js/2.c8eb3da9.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/raju-ahammad/React-ecommerce-frontend/static/js/2.c8eb3da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21694be47c8d4f5275c0",
+    "url": "/raju-ahammad/React-ecommerce-frontend/static/js/main.7206b7fa.chunk.js"
+  },
+  {
+    "revision": "f637b283d4916823d72a",
+    "url": "/raju-ahammad/React-ecommerce-frontend/static/js/runtime-main.b34939d0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/raju-ahammad/React-ecommerce-frontend/precache-manifest.e7b7774cbe1bd3a2b2e11f54f3ff6475.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raju-ahammad/React-ecommerce-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
